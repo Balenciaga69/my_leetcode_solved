e0fb41fa648516393e814ea081d9fe42a65f16df
KeyValuePair <zywzjsc@gmail.com>
2023-04-27 00:05:08 +0800

    no message

--- CSharp/forLeetCode/Program.cs
+++ CSharp/forLeetCode/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.Write
+﻿var nums = new int[] { 2, 7, 5, 11, 15 };
+var target = 9;
+var result = TwoSum(nums, target);
+
+Console.WriteLine($"[{result[0]}, {result[1]}]");
+
+static int[] TwoSum(int[] nums, int target)
+{
+    var result = new int[2];
+    var dict = new Dictionary<int, int>();
+    for (int i = 0; i < nums.Length; i++)
+    {
+        var complement = target - nums[i];
+        if (dict.ContainsKey(complement))
+        {
+            result[0] = dict[complement];
+            result[1] = i;
+            return result;
+        }
+        dict[nums[i]] = i;
+    }
+    return result;
+}
--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -1,1 +1,5 @@
+const x = { a: 5 }
+const y = {b:76}
+console.log('Hello')
+
 
