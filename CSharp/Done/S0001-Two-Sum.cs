1ceeba3be18e66a9af193959c3e19569d84f1310
KeyValuePair <zywzjsc@gmail.com>
2023-05-03 01:01:57 +0800

    no message

---
+++ CSharp/Done/S0001-Two-Sum.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+namespace Csharp.Done
+{
+    public class S0001_Two_Sum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var hTable = new Hashtable();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var nowNum = nums[i];
+                var goodNum = target - nowNum;
+                if (hTable.ContainsKey(goodNum))
+                {
+                    var rowValue = (int?)hTable[goodNum] ?? -1;
+                    return new int[] { rowValue, i };
+                }
+                else if (hTable.ContainsKey(nowNum) == false)
+                {
+                    hTable.Add(nowNum, i);
+                }
+            }
+            return new int[] { -1, -1 };
+        }
+    }
+}
