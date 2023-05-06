3657414e950244e36fa2f67a46ed9141aa4bf635
KeyValuePair <zywzjsc@gmail.com>
2023-05-07 00:19:21 +0800

    no message

---
+++ CSharp/Done/S0020-Valid-Parentheses.cs
@@ -0,0 +1,33 @@
+﻿namespace Csharp.Done
+{
+    public class S0020_Valid_Parentheses
+    {
+        public bool IsValid(string str)
+        {
+            char[] left = new char[] { '(', '[', '{' };
+            char[] right = new char[] { ')', ']', '}' };
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                char curr = str[i];
+                bool isInLeft = left.Contains(curr);
+                if (isInLeft) { stack.Push(curr); }
+                else
+                {
+                    if (stack.Count == 0) return false;
+                    int rightIdx = Array.IndexOf(right, curr);
+                    var peekLeft = stack.Peek();
+                    if (peekLeft != left[rightIdx])
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        stack.Pop();
+                    }
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
