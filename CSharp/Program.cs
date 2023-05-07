f886d05b99a455804b5613afdcd77edd9b8ec119
KeyValuePair <zywzjsc@gmail.com>
2023-05-07 12:26:40 +0800

    no message

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,30 +1,4 @@
 ﻿public class Solution
 {
-    public bool IsValid(string str)
-    {
-        char[] left = new char[] { '(', '[', '{' };
-        char[] right = new char[] { ')', ']', '}' };
-        Stack<char> stack = new Stack<char>();
-        for (int i = 0; i < str.Length; i++)
-        {
-            char curr = str[i];
-            bool isInLeft = left.Contains(curr);
-            if (isInLeft) { stack.Push(curr); }
-            else
-            {
-                if (stack.Count == 0) return false;
-                int rightIdx = Array.IndexOf(right, curr);
-                var peekLeft = stack.Peek();
-                if (peekLeft != left[rightIdx])
-                {
-                    return false;
-                }
-                else
-                {
-                    stack.Pop();
-                }
-            }
-        }
-        return stack.Count == 0;
-    }
+    
 }
