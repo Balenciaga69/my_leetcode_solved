7155d4d58579f06000766002ed73fc1ec821288e
KeyValuePair <zywzjsc@gmail.com>
2023-05-14 00:49:09 +0800

    no message

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,27 +1,14 @@
-﻿var solution = new Solution();
-int[] nums = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
-int val = 2;
-int result = solution.RemoveElement(nums, val);
-
-public class Solution
+﻿public class Solution
 {
-    public int RemoveElement(int[] nums, int val)
+    public int ClimbStairs(int n)
     {
-        var result = 0;
-        var left = 0;
-        var right = nums.Length - 1;
-        while (left < right)
+        var dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++)
         {
-            if (nums[left] == val)
-            {
-                nums[left] = nums[right];
-                right--;
-            }
-            else
-            {
-                left++;
-            }
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return right + 1;
+        return dp[n];
     }
 }
