7bd8af99e6295422546ce2447d05e5529f2eb897
KeyValuePair <zywzjsc@gmail.com>
2023-05-15 00:08:28 +0800

    no message

--- CSharp/Done/S0027-Remove-Element.cs
+++ CSharp/Done/S0027-Remove-Element.cs
@@ -1,24 +1,24 @@
 ﻿namespace Csharp.Done
 {
-    public class S0027_Remove_Element
-    {
-        public int RemoveElement(int[] nums, int val)
-        {
-            var left = 0;
-            var right = nums.Length - 1;
-            while (left <= right)
+   public class S0027_Remove_Element
+   {
+      public int RemoveElement(int[] nums, int val)
+      {
+         var left = 0;
+         var right = nums.Length - 1;
+         while (left <= right)
+         {
+            if (nums[left] == val)
             {
-                if (nums[left] == val)
-                {
-                    nums[left] = nums[right];
-                    right--;
-                }
-                else
-                {
-                    left++;
-                }
+               nums[left] = nums[right];
+               right--;
             }
-            return left;
-        }
-    }
+            else
+            {
+               left++;
+            }
+         }
+         return left;
+      }
+   }
 }
