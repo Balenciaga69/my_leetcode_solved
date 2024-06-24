e649c7692683c053c2449e1a37d76183f862f912
公司電腦 <zywzjsc@gmail.com>
2024-06-24 18:05:34 +0800

    no message

--- CSharp/Done/S0134-Gas-Station.cs
+++ CSharp/Done/S0134-Gas-Station.cs
@@ -4,40 +4,33 @@
     {
         public void Execute()
         {
-            var gas = new int[] { 1, 2, 3, 4, 5 };
+            var gas = new int[] { 0, 0, 0, 2, 13 };
+            //var gas = new int[] { 1, 2, 3, 4, 5 };
             var cost = new int[] { 3, 4, 5, 1, 2 };
-            var xxx = this.CanCompleteCircuit(gas, cost);
-            Console.WriteLine(xxx);
+            var ans = this.CanCompleteCircuit(gas, cost);
+            Console.WriteLine(ans);
         }
 
         public int CanCompleteCircuit(int[] gas, int[] cost)
         {
-            // 油耗大於加油量 直接 -1
-            if (cost.Sum() > gas.Sum())
-            {
-                return -1;
-            }
-
+            // 總油耗大於加油量 直接 -1
+            if (cost.Sum() > gas.Sum()) return -1;
+            var tank = 0;
+            var start = 0;
             for (int i = 0; i < gas.Length; i++)
             {
-                var remainingGas = 0;
-                var earnedGas = 0;
-                var j = i;
-                while (true)
+                // 剩餘油量 = 加油 - 到達下一站所需的消耗
+                tank = tank + gas[i] - cost[i];
+                if (tank < 0)
                 {
-                    // 加油中
-                    remainingGas += gas[j];
-                    earnedGas += gas[j];
-
-                    // 到達下一站
-                    remainingGas -= cost[j];
-                    j = j + 1 >= gas.Length ? 0 : j + 1;
-                    if (remainingGas < 0) break;
-                    if (earnedGas == gas.Sum()) return i;
+                    start = i + 1;
+                    tank = 0;
+                    continue;
                 }
             }
 
-            return -1;
+            // 當 燃料足夠的時候 總體一定能夠支撐整個旅程的油耗
+            return start;
         }
     }
 }
