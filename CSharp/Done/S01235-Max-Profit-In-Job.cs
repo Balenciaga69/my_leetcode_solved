98ca65fb52c11a5548729e5de8824b0765c4feb7
公司電腦 <zywzjsc@gmail.com>
2024-07-10 11:30:16 +0800

    no message

---
+++ CSharp/Done/S01235-Max-Profit-In-Job.cs
@@ -0,0 +1,22 @@
+﻿namespace Csharp.Done
+{
+    internal class S01235_Max_Profit_In_Job
+    {
+        public S01235_Max_Profit_In_Job()
+        {
+            var startTime = new int[] { 1, 2, 3, 4, 6 };
+            var endTime = new int[] { 3, 5, 10, 6, 9 };
+            var profit = new int[] { 20, 20, 100, 70, 60 };
+            this.JobScheduling(startTime, endTime, profit);
+        }
+
+        public int JobScheduling(int[] startTime, int[] endTime, int[] profit)
+        {
+            // TODO: 還沒完成
+            var max = 0;
+            Array.Sort(endTime);
+
+            return max;
+        }
+    }
+}
