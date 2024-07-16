abff33a5f57be3b23e8512f816289ec693380145
公司電腦 <zywzjsc@gmail.com>
2024-07-16 17:19:28 +0800

    no message

---
+++ CSharp/Solutions/S3210.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.Solutions
+{
+    internal class S3210
+    {
+        public S3210()
+        {
+            var s = "dart";
+            var k = 3;
+            this.GetEncryptedString(s, k);
+        }
+
+        public string GetEncryptedString(string s, int k)
+        {
+            var chars = s.Select((x, i) => s[(i + k) % s.Length]).ToArray();
+            var result = new string(chars);
+            return result;
+        }
+    }
+}
