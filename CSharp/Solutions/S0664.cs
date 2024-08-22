3396b3a1862ff60059aca16afd882f81b34d1c2d
公司電腦 <zywzjsc@gmail.com>
2024-08-22 14:00:31 +0800

    no message

---
+++ CSharp/Solutions/S0664.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.Solutions
+{
+    internal class S0664
+    {
+        public S0664()
+        {
+            this.StrangePrinter("AAABBB");
+        }
+
+        public int StrangePrinter(string s)
+        {
+            return 0;
+        }
+    }
+}
