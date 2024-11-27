a8a3773524e5e3bc16b1e3c3f7d646cc95bf75f8
公司電腦 <zywzjsc@gmail.com>
2024-11-27 17:04:56 +0800

    no message

--- Py/solved/s0344.py
+++ Py/solved/s0344.py
@@ -1,6 +1,18 @@
+from typing import List
+
+
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
-        
+        if len(s) == 0:
+            return
+        l = 0
+        r = len(s)-1
+        while l < r:
+            temp = s[l]
+            s[l] = s[r]
+            s[r] = temp
+            l += 1
+            r -= 1
