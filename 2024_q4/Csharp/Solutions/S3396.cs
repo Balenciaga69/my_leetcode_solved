3f15e21cb2167d6e97c21b05c6bf847be1c08908
737巷電腦 <zywzjsc@gmail.com>
2024-12-30 00:50:09 +0800

    no message

---
+++ 2024_q4/Csharp/Solutions/S3396.cs
@@ -0,0 +1,34 @@
+﻿namespace Csharp.Solutions.S3396
+{
+    public class Solution
+    {
+        public int MinimumOperations(int[] nums)
+        {
+            var dict = new Dictionary<int, int>();
+            var duplicatedSet = new HashSet<int>();
+            foreach (var x in nums)
+            {
+                var isContained = dict.TryGetValue(x, out var times);
+                if (isContained) dict[x]++;
+                else dict.Add(x, 1);
+                if (dict[x] > 1 && !duplicatedSet.Contains(x)) duplicatedSet.Add(x);
+            }
+
+            if (duplicatedSet.Count == 0) return 0;
+            var idx = 0;
+            var deleteTimes = 0;
+            while (true)
+            {
+                deleteTimes++;
+                for (var i = 1; i <= 3; i++)
+                {
+                    if (idx == nums.Length) return deleteTimes;
+                    var n = nums[idx++];
+                    dict[n]--;
+                    if (dict[n] < 2) duplicatedSet.Remove(n);
+                    if (duplicatedSet.Count == 0) return deleteTimes;
+                }
+            }
+        }
+    }
+}
---
+++ 2024_q4/Csharp/Solutions/S3398.cs
@@ -0,0 +1,32 @@
+﻿namespace Csharp.Solutions.S3398
+{
+    public class Solution
+    {
+        public int MaxDistinctElements(int[] nums, int k)
+        {
+            // 選最小 且沒人選過的數字
+            Array.Sort(nums);
+            var set = new HashSet<int>();
+            var nextStart = Int32.MinValue;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var n = nums[i];
+
+                // 不做重複無謂的計算
+                var start = Math.Max(n - k, nextStart);
+                for (int val = start; val <= n + k; val++)
+                {
+                    if (!set.Contains(val))
+                    {
+                        set.Add(val);
+                        nextStart = val + 1;
+                        break;
+                    }
+                }
+            }
+
+            return set.Count;
+        }
+    }
+}
