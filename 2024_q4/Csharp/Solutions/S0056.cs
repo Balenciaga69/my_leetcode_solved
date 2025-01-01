0eac94c2a6775e75b53d8486ac3b3bb4c898618a
737巷電腦 <zywzjsc@gmail.com>
2025-01-02 01:15:00 +0800

    no message

--- 2024_q4/Csharp/Solutions/S0056.cs
+++ 2024_q4/Csharp/Solutions/S0056.cs
@@ -1,15 +1,19 @@
-﻿namespace Csharp.Solutions.S0056;
+﻿/*
+ * 56. Merge Intervals
+ * Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+ */
 
+namespace Csharp.Solutions.S0056;
 public class Solution
 {
     public Solution()
     {
         var intervals = new int[][]
          {
+            new int[] { 15, 18 },
             new int[] { 1, 3 },
             new int[] { 2, 6 },
             new int[] { 8, 10 },
-            new int[] { 15, 18 },
          };
 
         this.Merge(intervals);
@@ -17,7 +21,31 @@ public class Solution
 
     public int[][] Merge(int[][] intervals)
     {
-        var merged = new List<int[][]>();
+        if (intervals.Length < 2) return intervals;
+        Array.Sort(intervals, (a, b) => a[0] - b[0]);
+        var merged = new List<int[]>();
+        merged.Add(intervals[0]);
+        foreach (var currItem in intervals)
+        {
+            var lastItem = merged.Last();
+
+            if (lastItem[1] >= currItem[0]) // 在區間內
+            {
+                // 邊界比原本的大
+                if (currItem[1] > lastItem[1])
+                {
+                    // 擴增範圍
+                    var tempItem = new int[] { lastItem[0], currItem[1] };
+                    merged[merged.Count - 1] = tempItem;
+                }
+            }
+            else // 在區間外
+            {
+                merged.Add(currItem);
+            }
+        }
+
+        return merged.ToArray();
     }
 }
 
