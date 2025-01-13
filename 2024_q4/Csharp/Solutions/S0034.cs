ba96d96422132ace4eda9a2cd902875cb651a8c2
公司電腦 <zywzjsc@gmail.com>
2025-01-13 18:18:08 +0800

    no message

---
+++ 2024_q4/Csharp/Solutions/S0034.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.Solutions.S0034;
+
+public class Solution
+{
+    public Solution()
+    {
+        var nums = new int[] { 5, 7, 8, 8, 8, 8, 8, 9 };
+        this.SearchRange(nums, 8);
+    }
+
+    public int[] SearchRange(int[] nums, int target)
+    {
+        var l = 0;
+        var r = nums.Length - 1;
+        var pivot = -1;
+        while (l <= r)
+        {
+            var m = (l + r) / 2;
+            if (nums[m] == target)
+            {
+                pivot = m;
+                break;
+            }
+
+            if (nums[m] > target) r = m - 1;
+            else l = m + 1;
+        }
+
+        if (pivot == -1) return new[] { -1, -1 };
+        var maxIdx = FindMaxIdx(nums, target, pivot, pivot, nums.Length - 1);
+        var minIdx = FindMinIdx(nums, target, pivot, 0, pivot);
+        return new int[] { minIdx, maxIdx };
+    }
+
+    private static int FindMinIdx(int[] nums, int target, int minIdx, int l, int r)
+    {
+        while (l <= r)
+        {
+            var m = (l + r) / 2;
+            if (nums[m] == target)
+            {
+                minIdx = m;
+                r = m - 1;
+                continue;
+            }
+
+            if (nums[m] > target) r = m - 1;
+            else l = m + 1;
+        }
+
+        return minIdx;
+    }
+
+    private static int FindMaxIdx(int[] nums, int target, int maxIdx, int l, int r)
+    {
+        while (l <= r)
+        {
+            var m = (l + r) / 2;
+            if (nums[m] == target)
+            {
+                maxIdx = m;
+                l = m + 1;
+                continue;
+            }
+
+            if (nums[m] > target) r = m - 1;
+            else l = m + 1;
+        }
+
+        return maxIdx;
+    }
+
+}
---
+++ 2024_q4/Csharp/Solutions/S0704.cs
@@ -0,0 +1,34 @@
+﻿namespace Csharp.Solutions.S0704;
+
+public class Solution
+{
+    public Solution()
+    {
+        var nums = new int[] { -1, 0, 3, 5, 9, 12 };
+        var target = 2;
+        var result = this.Search(nums, target);
+        Console.WriteLine(result);
+    }
+
+    public int Search(int[] nums, int target)
+    {
+        var l = 0;
+        var r = nums.Length - 1;
+
+        while (l <= r)
+        {
+            var m = (l + r) / 2;
+            if (nums[m] == target) return m;
+            if (nums[m] > target)
+            {
+                r = m - 1;
+            }
+            else
+            {
+                l = m + 1;
+            }
+        }
+
+        return -1;
+    }
+}
