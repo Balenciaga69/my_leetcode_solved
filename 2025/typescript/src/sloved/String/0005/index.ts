42c468bf7dd38d3afaf21b89542829c6f3baf6b9
737 Server <zywzjsc@gmail.com>
2025-11-01 23:58:17 +0800

    0005

--- 2025/typescript/src/sloved/String/0005/index.ts
+++ 2025/typescript/src/sloved/String/0005/index.ts
@@ -1,4 +1,5 @@
 ﻿/*
+#1 2025-11-01 [20mins]
 ---
 5. 最长回文子串
 給你一個字串 s，請你找出其中 最長的回文子字串（palindromic substring），並回傳它。
@@ -21,5 +22,26 @@
 s 只包含英文字母與數字
 */
 export function longestPalindrome(s: string): string {
-    // 未完成
-};
+  const n = s.length
+  if (n <= 1) return s
+
+  let gl_str = s[0]
+
+  const exec = (left: number, right: number) => {
+    while (left >= 0 && right < n && s[left] === s[right]) {
+      const currStr = s.substring(left, right + 1)
+      const curLen = currStr.length
+      if (curLen > gl_str.length) gl_str = currStr
+      left--
+      right++
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    exec(i - 1, i + 1)
+    if (i + 1 < n && s[i] === s[i + 1]) {
+      if (gl_str.length < 2) gl_str = s.substring(i, i + 2)
+      exec(i - 1, i + 2)
+    }
+  }
+  return gl_str
+}
